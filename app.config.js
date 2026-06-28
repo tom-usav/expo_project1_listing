134983569061df060534908c35ebb0237e64e8c9
Thomas Vo <thomasvo@Thomass-Mac-mini.local>
2026-06-28 17:24:05 -0400

    add port 3001

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ const appJson = require('./app.json');
 require('dotenv').config();
 
 module.exports = () => {
-  const apiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+  const apiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3001';
 
   return {
     ...appJson,
